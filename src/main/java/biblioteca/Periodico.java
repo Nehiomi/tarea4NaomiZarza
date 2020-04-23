4d96b0ab32f1c15727ca549db8129fbe6a89fc3b
nao <zarza.naomi@gmail.com>
2020-04-23 18:37:09 +0200

    Añade atributos en clase modelo

---
+++ src/main/java/biblioteca/Periodico.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biblioteca;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Nao
+ */
+public class Periodico {
+    
+    //Atributos
+    private String nombre;
+    private double precio;
+    private Date fecha;
+}
