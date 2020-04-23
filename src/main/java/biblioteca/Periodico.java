e46847642a65f8d82a5b0f46575b60c1324f9675
nao <zarza.naomi@gmail.com>
2020-04-23 19:07:12 +0200

    Añade métodos getters y setters a la clase Periodico

--- src/main/java/biblioteca/Periodico.java
+++ src/main/java/biblioteca/Periodico.java
@@ -12,23 +12,49 @@ import java.time.LocalDate;
  * @author Nao
  */
 public class Periodico {
-    
+
     //Atributos
     private String nombre;
     private double precio;
     private LocalDate fecha;
-    
+
     //Constructor parametrizado
     public Periodico(String nombre, double precio, LocalDate fecha) {
         this.nombre = nombre;
         this.precio = precio;
         this.fecha = fecha;
     }
-    
+
     //Constructor por defecto
-     public Periodico() {
+    public Periodico() {
         this.nombre = "El País";
         this.precio = 3.2;
         this.fecha = LocalDate.now();
     }
+
+    //Métodos getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
 }
